94a2694b5d6d92a9991262db0b56cc9a9689f5b6
zhidali <zhidali43@gmail.com>
2022-11-28 20:19:59 -0800

    update log-loss plot

--- src/playground/gbdt_offline_sample/gbm_loss_plot.py
+++ src/playground/gbdt_offline_sample/gbm_loss_plot.py
@@ -2,7 +2,7 @@
     @authors Zhida Li
     @email zhidal@sfu.ca
     @date Nov. 16, 2022
-    @version: 1.0.0
+    @version: 1.0.1
     @description:
                 GBDT: loss plotting
     @copyright Copyright (c) Nov. 16, 2022
@@ -15,7 +15,7 @@
 # ==============================================
 # File: For plotting GBDT loss
 # ==============================================
-# Last modified: Nov. 16, 2022
+# Last modified: Nov. 28, 2022
 
 # Import Python libraries
 import sys
@@ -30,9 +30,11 @@ import matplotlib.pyplot as plt
 
 from sklearn import metrics
 from sklearn.metrics import f1_score
-# import lightgbm as lbg
+
+# from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+# from catboost import CatBoostClassifier
+# from sklearn.ensemble import GradientBoostingClassifier
 
 # Import customized libraries
 from journalLib.replaceNan import replaceNan
@@ -60,11 +62,32 @@ Yy = test_y
 NUM_ESTIMATORS = 300
 LEARN_RATE = 0.01
 
-eval_set_train_valid = [(X, Y.ravel()), (Xx, Yy.ravel())]
+# GBM algorithm
+# algo_gbm = 'lightgbm'  # xgboost, lightgbm, catboost
+print('Choose a GBM: xgboost, lightgbm, catboost')
+algo_gbm =  input('GBM: ')
+
+algo_list = ['xgboost', 'lightgbm', 'catboost']
+if algo_gbm not in algo_list:
+    print("Please re-enter the GBM.")
+    exit()
+
 # Training
 time_start = time.time()
-gbm = LGBMClassifier(learning_rate=LEARN_RATE, n_estimators=NUM_ESTIMATORS)
-gbm.fit(X, Y.ravel(), eval_set=eval_set_train_valid, eval_metric='logloss')
+
+if algo_gbm == 'xgboost':
+    gbm = XGBClassifier(learning_rate=LEARN_RATE, n_estimators=NUM_ESTIMATORS, max_depth=6, subsample=1,
+                        booster='gbtree')  #
+elif algo_gbm == 'lightgbm':
+    gbm = LGBMClassifier(learning_rate=LEARN_RATE, n_estimators=NUM_ESTIMATORS)
+    # gbm.fit(X, Y.ravel(), eval_set=eval_set_train_valid, eval_metric='logloss')
+
+elif algo_gbm == 'catboost':
+    gbm = CatBoostClassifier(learning_rate=LEARN_RATE, n_estimators=NUM_ESTIMATORS, max_depth=6, subsample=1, num_leaves=31,
+                             boosting_type='Plain')  # or Ordered, Plain type
+
+eval_set_train_valid = [(X, Y.ravel()), (Xx, Yy.ravel())]
+gbm.fit(X, Y.ravel(), eval_set=eval_set_train_valid)
 # gbm.fit(X, Y.ravel())
 time_end = time.time()
 
@@ -81,14 +104,29 @@ print('Accuracy:', accuracy_gbm, '\n',
       'Training time:', trainingTime_gbm)
 
 # Plotting logloss: -log P(yt|yp) = -(yt log(yp) + (1 - yt) log(1 - yp))
-results = gbm.evals_result_  # dict for the 'valid_0', 'binary_logloss'
-epochs = len(results['valid_0']['binary_logloss'])
+results = gbm.evals_result_  # dict
+if algo_gbm == 'xgboost':
+    epochs = len(results['validation_0']['logloss'])
+elif algo_gbm == 'lightgbm':
+    epochs = len(results['valid_0']['binary_logloss'])
+elif algo_gbm == 'catboost':
+    epochs = len(results['validation_0']['Logloss'])
+
 x_axis = range(0, epochs)
 # plot log loss
-plt.plot(x_axis, results['valid_0']['binary_logloss'], label='Train')
-plt.plot(x_axis, results['valid_1']['binary_logloss'], label='Valid')
+if algo_gbm == 'xgboost':
+    plt.plot(x_axis, results['validation_0']['logloss'], label='Train')
+    plt.plot(x_axis, results['validation_1']['logloss'], label='Valid')
+elif algo_gbm == 'lightgbm':
+    plt.plot(x_axis, results['valid_0']['binary_logloss'], label='Train')
+    plt.plot(x_axis, results['valid_1']['binary_logloss'], label='Valid')
+elif algo_gbm == 'catboost':
+    plt.plot(x_axis, results['validation_0']['Logloss'], label='Train')
+    plt.plot(x_axis, results['validation_1']['Logloss'], label='Valid')
+
 plt.legend()
 plt.xlabel('Number of estimators')
 plt.ylabel('Log Loss')
-plt.title('LightGBM Log Loss')
+plt.title('%s Log Loss' % algo_gbm)
 plt.show()
+
